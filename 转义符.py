1b5db7f1c78d6e0cb1345bf3cc987675399095c8
王玉 <wangyu.oicq@163.com>
2018-01-03 11:57:20 +0800

    转义符

---
+++ 转义符.py
@@ -0,0 +1,19 @@
+print('I\'m ok.')
+
+print('I\'m learning\nPython')
+
+print('\\\n\\')
+
+print('\\\t\\\n\\\t\\')
+
+print('\\\\\\t\\\\')
+
+print(r'\\\t\\')
+
+print('\\\t\\')
+
+print('''wangyu
+wangqian''')
+
+print(r'''wangyu
+wangqian''')
